9c56884a26295321b32d40ad8419d964f9833c0d
ari123aji <65709320+ari123aji@users.noreply.github.com>
2021-10-05 16:17:47 +0700

    Update main.blade.php

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -12,12 +12,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
         <!-- Custom styles for this template -->
-    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href="../css/dashboard.css" rel="stylesheet">
         <!-- styles for this-->
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
   <!-- trix -->
-    <link rel="stylesheet" type="text/css" href="/css/trix.css">
-  <script type="text/javascript" src="/js/trix.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/trix.css">
+  <script type="text/javascript" src="../js/trix.js"></script>
 
   <style>
     trix-toolbar [data-trix-button-group="file-tools"]{
