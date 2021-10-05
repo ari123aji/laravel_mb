90e2c054a6c3642ab69a96604041909d283c545c
ari123aji <65709320+ari123aji@users.noreply.github.com>
2021-10-05 16:28:35 +0700

    Update main.blade.php

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -6,10 +6,10 @@
     <title>{{ $title }}</title>
 
     <!-- google font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Sofia">
 
     <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
         <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
